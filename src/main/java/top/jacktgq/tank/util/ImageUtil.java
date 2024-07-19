b7ddbfb4f99eed0cf65da4f9ebcbe1d91e7bb801
Nahjs <23301132@bjtu.edu.cn>
2024-07-19 20:24:48 +0800

    i

--- src/main/java/top/jacktgq/tank/util/ImageUtil.java
+++ src/main/java/top/jacktgq/tank/util/ImageUtil.java
@@ -3,22 +3,34 @@ package top.jacktgq.tank.util;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+/**
+ * 图像处理工具类
+ */
 
 public class ImageUtil {
-	public static BufferedImage rotateImage(final BufferedImage bufferedimage,
-            final int degree) {
-        int w = bufferedimage.getWidth();
-        int h = bufferedimage.getHeight();
-        int type = bufferedimage.getColorModel().getTransparency();
-        BufferedImage img;
-        Graphics2D graphics2d;
-        (graphics2d = (img = new BufferedImage(w, h, type))
-                .createGraphics()).setRenderingHint(
-                RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        graphics2d.rotate(Math.toRadians(degree), w / 2, h / 2);
-        graphics2d.drawImage(bufferedimage, 0, 0, null);
-        graphics2d.dispose();
-        return img;
-    }
+
+        // 旋转给定的图像指定的角度
+        public static BufferedImage rotateImage(final BufferedImage bufferedimage,
+                                                final int degree) {
+                //获取原始图像的宽度和高度
+                int w = bufferedimage.getWidth();
+                int h = bufferedimage.getHeight();
+                //获取图像的透明度类型
+                int type = bufferedimage.getColorModel().getTransparency();
+
+                // 创建新的BufferedImage对象：
+                BufferedImage img;
+
+                // 创建Graphics2D对象：
+                Graphics2D graphics2d;
+                (graphics2d = (img = new BufferedImage(w, h, type))//新的图像上绘制内容
+                        .createGraphics()).setRenderingHint(//设置了图像渲染时使用的插值方法
+                        RenderingHints.KEY_INTERPOLATION,
+                        RenderingHints.VALUE_INTERPOLATION_BILINEAR);//使用双线性插值，它可以在旋转图像时提供更平滑的视觉效果。
+                graphics2d.rotate(Math.toRadians(degree), w / 2, h / 2);//将Graphics2D对象的坐标系统旋转指定的角度。旋转的中心点是图像的中心
+                graphics2d.drawImage(bufferedimage, 0, 0, null);//将原始图像绘制到新的图像对象上
+                graphics2d.dispose();//释放Graphics2D对象的资源
+                return img;
+        }
 }
+
--- src/main/java/top/jacktgq/tank/util/LogUtils.java
+++ src/main/java/top/jacktgq/tank/util/LogUtils.java
@@ -4,16 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * 
- * @Title: TimeUtils.java 
- * @Package top.jacktgq 
- * @Description: 自定义日志打印类
- * @author CandyWall   
- * @date 2020年11月1日 下午7:35:23 
- * @version V1.0
+ *日志打印类
  */
 public class LogUtils {
-	
+
 	/**
 	 * 获取当前系统时间，并进行格式化
 	 */
@@ -21,16 +15,16 @@ public class LogUtils {
 		LocalDateTime now = LocalDateTime.now();
 		return now.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss:SSS"));
 	}
-	
+
 	/**
 	 * @param info ：要输出的内容
 	 */
 	public static void log(String info) {
 		log("", info);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param className	：类名
 	 * @param info		：要输出的内容
 	 */
